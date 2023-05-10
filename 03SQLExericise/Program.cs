9ec7ae8d5082c640c282813dceebca3abfe6d3da
Michal <m.stefanski98@gmail.com>
2023-05-10 14:11:07 +0200

    ORM in use

--- 03SQLExericise/Program.cs
+++ 03SQLExericise/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello");
+﻿using _03SQLExericise.Models;
+
+VolleyballDatabaseContext db = new VolleyballDatabaseContext();
+
+Person[] people = db.Persons.ToArray();
+
+foreach (Person person in people)
+{
+    Console.WriteLine(person.FirstName + " " +person.LastName);
+}
