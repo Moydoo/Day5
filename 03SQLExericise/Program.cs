b1b273632c5eabec3388341dd4f5624f59759b11
Michal <m.stefanski98@gmail.com>
2023-05-10 15:02:41 +0200

    Database first approach

--- 03SQLExericise/Program.cs
+++ 03SQLExericise/Program.cs
@@ -19,24 +19,24 @@ Person[] people = db.Persons.ToArray();
 
 
 
-Person person = new Person()
-{
-    FirstName = "Gimme",
-    LastName = "Rimmi"
-};
+//Person person = new Person()
+//{
+//    FirstName = "Gimme",
+//    LastName = "Rimmi"
+//};
 
 
-try
-{
-    db.Persons.Add(person);
-    db.SaveChanges();
-    Console.WriteLine("Created successfully");
-}
+//try
+//{
+//    db.Persons.Add(person);
+//    db.SaveChanges();
+//    Console.WriteLine("Created successfully");
+//}
 
-catch (SqlException)
-{
-    Console.WriteLine("No no no");
-}
+//catch (SqlException)
+//{
+//    Console.WriteLine("No no no");
+//}
 
 
 //UPDATE
---
+++ 04SQLExercise/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04SQLExercise
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Coach> Coaches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MyDatabase;Integrated Security = True; ");
+        }
+    }
+}
