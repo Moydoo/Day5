6bae432ab0cf5e38377c3269df0cfe9b6588a91b
Michal <m.stefanski98@gmail.com>
2023-05-10 12:05:46 +0200

    Person repository

--- 2SQLRepositories/PersonsRepository.cs
+++ 2SQLRepositories/PersonsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,33 @@ namespace _2SQLRepositories
 {
     internal class PersonsRepository
     {
-        public Array GetPersons()
+        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=VolleyballDatabase;Integrated Security=True;";
+       
+        
+        //SELECTING THE VALUES
+        public Person[] GetPersons()
         {
 
+            List<Person> persons = new List<Person>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT Id, FirstName, LastName FROM Persons", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Person person = new Person();
+                            person.Id = reader.GetInt32(0);
+                            person.FirstName = reader.GetString(1);
+                            person.LastName = reader.GetString(2);
+                            persons.Add(person);
+                        }
+                    }
+                }
+            }
+            return persons.ToArray();
         }
     }
 }
--- 2SQLRepositories/Program.cs
+++ 2SQLRepositories/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _2SQLRepositories;
+
+PersonsRepository personsRepository = new PersonsRepository();
+
+Person[] people = personsRepository.GetPersons();
+
+foreach (var p in people)
+{
+    Console.WriteLine($"Farst name is {p.FirstName} and last name is {p.LastName}");
+}
