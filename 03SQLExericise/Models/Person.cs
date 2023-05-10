25a9672df9f6eb683d7f2f3e225e54d85a0d6cea
Michal <m.stefanski98@gmail.com>
2023-05-10 14:00:04 +0200

    downloading packages and generating entieies

---
+++ 03SQLExericise/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03SQLExericise.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+}
